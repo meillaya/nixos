2424b3d8b6d771adc30e61b1e0e703fcecb8b933
Meillaya <nathanagbomed@proton.me>
2026-09-02 10:02:06 -0400

    fix(nixos): trust nix-community cachix public key

--- modules/nixos/system.nix
+++ modules/nixos/system.nix
@@ -48,6 +48,7 @@ in
       ];
       trusted-public-keys = [
         "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       ];
     };
