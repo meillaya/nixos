03222df141fc2ee55b0eefbfe1d4206421f3d1be
Meillaya <nathanagbomed@proton.me>
2026-09-02 10:02:06 -0400

    feat(darwin): trust noctalia binary cache

--- modules/darwin/base.nix
+++ modules/darwin/base.nix
@@ -9,8 +9,13 @@ in
 
     settings = {
       trusted-users = [ "@admin" username ];
-      substituters = [ "https://nix-community.cachix.org" "https://cache.nixos.org" ];
+      substituters = [
+        "https://noctalia.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://cache.nixos.org"
+      ];
       trusted-public-keys = [
+        "noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
     };
